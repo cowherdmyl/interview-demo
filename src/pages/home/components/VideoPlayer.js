ebc718b69ebe85ac9f3f7fee1e32e02b9cc20e44
门玉亮 <yuliang.men@tcl.com>
2024-05-28 15:53:05 +0800

    feat: video comp

---
+++ src/pages/home/components/VideoPlayer.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from 'react'
+import videoSource from '../../../assets/intro.mp4'
+
+function VideoPlayer() {
+  // 使用 useRef 创建一个 ref 对象，用于引用视频元素
+  const videoRef = useRef(null)
+
+  useEffect(() => {
+    // 检查 videoRef 是否指向一个 DOM 元素
+    if (videoRef.current) {
+      videoRef.current.muted = true // 确保视频静音
+      videoRef.current
+        .play()
+        .then(() => {
+          // 自动播放开始
+          console.log('视频自动播放已开始。')
+        })
+        .catch(error => {
+          // 自动播放失败
+          console.error('视频自动播放失败：', error)
+        })
+    }
+  }, []) // 空依赖数组意味着这个 useEffect 只在组件挂载时执行一次
+
+  return (
+    <video ref={videoRef} loop playsInline>
+      <source src={videoSource} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  )
+}
+
+export default VideoPlayer
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,19 +1,10 @@
 import React from 'react'
-import myVideo from '../../assets/1.mp4'
+import VideoPlayer from './components/VideoPlayer'
 
 function App() {
   return (
     <div className="App">
-      {/* <video autoPlay muted loop preload="auto">
-        <source src="https://cowherdmyl-6g4qp1up02368618-1317320311.tcloudbaseapp.com/intro.mp4" type="video/mp4" />
-        Your browser does not support HTML5 video.
-      </video> */}
-      <video src="https://cowherdmyl-6g4qp1up02368618-1317320311.tcloudbaseapp.com/intro.mp4" controls>
-        Your browser does not support the video tag.
-      </video>
-      <video src={myVideo} controls>
-        Your browser does not support the video tag.
-      </video>
+      <VideoPlayer />
     </div>
   )
 }
