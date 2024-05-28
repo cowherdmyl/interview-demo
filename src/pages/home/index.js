44517af91b0025746d123513b37c23ba8669c5d2
门玉亮 <yuliang.men@tcl.com>
2024-05-28 15:25:03 +0800

    feat: test video

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,9 +4,12 @@ import React from 'react'
 function App() {
   return (
     <div className="App">
-      <video autoPlay muted loop preload="auto">
+      {/* <video autoPlay muted loop preload="auto">
         <source src="https://cowherdmyl-6g4qp1up02368618-1317320311.tcloudbaseapp.com/intro.mp4" type="video/mp4" />
         Your browser does not support HTML5 video.
+      </video> */}
+      <video src="https://cowherdmyl-6g4qp1up02368618-1317320311.tcloudbaseapp.com/intro.mp4" controls>
+        Your browser does not support the video tag.
       </video>
     </div>
   )
