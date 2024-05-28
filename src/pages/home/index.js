56dc07c19d95566b14b7db3a77bbbef338f5c7bd
门玉亮 <yuliang.men@tcl.com>
2024-05-28 14:32:26 +0800

    feat: test url

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import myVideo from '../../assets/intro.mp4'
+// import myVideo from '../../assets/intro.mp4'
 
 function App() {
   return (
     <div className="App">
       <video autoPlay muted loop preload="auto">
-        <source src={myVideo} type="video/mp4" />
+        <source src="https://cowherdmyl-6g4qp1up02368618-1317320311.tcloudbaseapp.com/intro.mp4" type="video/mp4" />
         Your browser does not support HTML5 video.
       </video>
     </div>
