8d80bae452bc505def18f842dea59aabefde6518
门玉亮 <yuliang.men@tcl.com>
2024-05-31 12:31:18 +0800

    feat: 优化首页动画

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -62,13 +62,12 @@ function App() {
 
   const handleScroll = throttle(
     target => {
-      console.log('scroll')
       if (target === 'secondScreen') {
         // 直接操作DOM hidden 第二屏幕和过渡盒TransitionBox
         // transitionBoxRef.current.style.display = 'block'
         // secondScreenRef.current.style.display = 'block'
         setIsToggled(true)
-        smoothScrollTo(appContainerRef.current, secondScreenRef.current, 2000).then(() => {
+        smoothScrollTo(appContainerRef.current, secondScreenRef.current, 1500).then(() => {
           // firstScreenRef.current.style.display = 'none'
           // transitionBoxRef.current.style.display = 'none'
           setCurrentScreen('secondScreen')
@@ -78,7 +77,7 @@ function App() {
         // firstScreenRef.current.style.display = 'block'
         // transitionBoxRef.current.style.display = 'block'
         setIsToggled(true)
-        smoothScrollTo(appContainerRef.current, firstScreenRef.current, 2000).then(() => {
+        smoothScrollTo(appContainerRef.current, firstScreenRef.current, 1500).then(() => {
           // transitionBoxRef.current.style.display = 'none'
           // secondScreenRef.current.style.display = 'none'
           setCurrentScreen('firstScreen')
